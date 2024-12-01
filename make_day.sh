4778286cae7ec3e7a121240c8b918f5db79ee126
Brian Starkey <stark3y@gmail.com>
2024-12-01 09:11:43 +0000

    Add helper scripts

---
+++ make_day.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+DAY=$(printf %d $1)
+ZERO_DAY=$(printf %02d $1)
+
+mkdir ${ZERO_DAY}
+
+curl -b "session=${AOC_SESSION}" -o ${ZERO_DAY}/input.txt https://adventofcode.com/2024/day/${DAY}/input
+
+tail -n 3 ${ZERO_DAY}/input.txt
+
+if [ ! -f ${ZERO_DAY}/solve.py ]
+then
+	cp template.py ${ZERO_DAY}/solve.py
+fi
