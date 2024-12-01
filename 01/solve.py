fdaef059ffacde029d9a6c4137c818b93964d30d
Brian Starkey <stark3y@gmail.com>
2024-12-01 08:32:18 +0000

    01: part 2

--- 01/solve.py
+++ 01/solve.py
@@ -1,5 +1,7 @@
 import sys
 
+from collections import defaultdict
+
 a_list, b_list = [], []
 with open(sys.argv[1]) as f:
     for line in f:
@@ -8,10 +10,24 @@ with open(sys.argv[1]) as f:
         a_list.append(int(a))
         b_list.append(int(b))
 
+    # First sort both lists
     a_list.sort()
     b_list.sort()
 
+    # Then get their pairwise differences
     diff_list = []
     for a, b in zip(a_list, b_list):
         diff_list.append(abs(b - a))
-    print(sum(diff_list))
+
+    part1 = sum(diff_list)
+    print(part1)
+
+    # Track frequency histogram
+    hist = defaultdict(int)
+    for v in b_list:
+        hist[v] += 1
+
+    products = map(lambda a: a * hist[a], a_list)
+
+    part2 = sum(products)
+    print(part2)
