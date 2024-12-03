7738e456c17004bd4a2f9d5b8ed6f61ab4ed453a
Brian Starkey <stark3y@gmail.com>
2024-12-03 08:04:35 +0000

    02: part2

--- 03/solve.py
+++ 03/solve.py
@@ -1,14 +1,29 @@
 import re
 import sys
 
-mul_re = re.compile(r"mul\(([0-9]+),([0-9]+)\)")
+mul_re_str = r"(mul)\(([0-9]+),([0-9]+)\)"
+do_re_str = r"(do)\(\)"
+dont_re_str = r"(don\'t)\(\)"
+
+full_re = re.compile(f"({do_re_str}|{dont_re_str}|{mul_re_str})")
 
 part1 = 0
+part2 = 0
 with open(sys.argv[1]) as f:
     program = f.read()
 
-    matches = mul_re.findall(program)
+    do = True
+
+    matches = full_re.finditer(program)
     for match in matches:
-        a, b = map(int, match)
-        part1 += a * b
+        if match[2] == "do":
+            do = True
+        elif match[3] == "don't":
+            do = False
+        elif match[4] == "mul":
+            product = int(match[5]) * int(match[6])
+            part1 += product
+            if do:
+                part2 += product
 print(part1)
+print(part2)
