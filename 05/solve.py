b50eb15775fe6d123dff0d46b232a2b89014d8c0
Brian Starkey <stark3y@gmail.com>
2024-12-05 08:30:40 +0000

    05: part2

--- 05/solve.py
+++ 05/solve.py
@@ -9,7 +9,26 @@ def check_update(update):
                 return False
     return True
 
+
+
+def rebuild_update(fields):
+    update = []
+
+    for f in fields:
+        applies = filter(lambda v: v[0] == f, rules)
+        insert_idx = len(update)
+        for a, b in applies:
+            try:
+                idx = update.index(b)
+                insert_idx = min(idx, insert_idx)
+            except ValueError:
+                pass
+        update.insert(insert_idx, f)
+
+    return update
+
 part1 = 0
+part2 = 0
 
 with open(sys.argv[1]) as f:
     lines = map(str.strip, f)
@@ -19,8 +38,6 @@ with open(sys.argv[1]) as f:
 
         a, b = map(int, line.split("|"))
         rules.append((a, b))
-
-    print(f"{len(rules)=}")
 
     for line in lines:
         fields = list(map(int, line.split(",")))
@@ -35,5 +52,10 @@ with open(sys.argv[1]) as f:
         if check_update(update):
             val = fields[(len(fields))//2]
             part1 += val
+        else:
+            new_fields = rebuild_update(fields)
+            val = new_fields[(len(new_fields))//2]
+            part2 += val
 
 print(part1)
+print(part2)
