08021e73356d30256b8a1dfb10f5c770ceab4958
Brian Starkey <stark3y@gmail.com>
2024-12-08 09:26:55 +0000

    08: part 2

--- 08/solve.py
+++ 08/solve.py
@@ -27,18 +27,35 @@ def in_bounds(an):
     return True
 
 antinodes = set()
+antinodes_p2 = set()
 
 for freq, ant in antennas.items():
     for pair in combinations(ant, 2):
-        dr, dc = (pair[1][0] - pair[0][0]), (pair[1][1] - pair[0][1])
+        dc, dr = (pair[1][0] - pair[0][0]), (pair[1][1] - pair[0][1])
 
-        an1 = (pair[0][0] - dr, pair[0][1] - dc)
-        an2 = (pair[1][0] + dr, pair[1][1] + dc)
+        an1 = (pair[0][0] - dc, pair[0][1] - dr)
+        an2 = (pair[1][0] + dc, pair[1][1] + dr)
 
         if in_bounds(an1):
             antinodes.add(an1)
         if in_bounds(an2):
             antinodes.add(an2)
 
+        # Theres definitely two antennas in-line (they're a pair)
+        # so they both count as part 2 antinodes
+        antinodes_p2.add(pair[0])
+        antinodes_p2.add(pair[1])
+
+        while in_bounds(an1):
+            antinodes_p2.add(an1)
+            an1 = (an1[0] - dc, an1[1] - dr)
+
+        while in_bounds(an2):
+            antinodes_p2.add(an2)
+            an2 = (an2[0] + dc, an2[1] + dr)
+
 part1 = len(antinodes)
 print(part1)
+
+part2 = len(antinodes_p2)
+print(part2)
