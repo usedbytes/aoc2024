46f1d50d087e930e9d5e005f25bdc4b9e5f79cd1
Brian Starkey <stark3y@gmail.com>
2024-12-09 19:21:01 +0000

    09: part 1 take 2
    Much slower but more flexible implementation

--- 09/solve.py
+++ 09/solve.py
@@ -3,58 +3,55 @@ import sys
 with open(sys.argv[1]) as f:
     diskmap = f.read().strip()
 
-checksum = 0
-head_id = 0
-n_files = (len(diskmap) + 1) // 2
-tail_id = n_files
-head_out_idx = 0
-tail_cursor = n_files * 2
+blocks = []
 
-tail_remaining = 0
-
-disk = []
-
+# Build a list of blocks
+file_id = 0
+start_idx = 0
 for i, v in enumerate(diskmap):
-    v = int(v)
-
-    if i > tail_cursor:
-        break
-
+    size = int(v)
     if i % 2 == 0:
-        # File
-
-        # This file has already been partially moved, reduce size accordingly
-        if head_id == tail_id:
-            v = tail_remaining
-
-        for j in range(v):
-            checksum += head_out_idx * head_id
-            head_out_idx += 1
-
-        head_id += 1
+        blocks.append([file_id, start_idx, size])
+        file_id += 1
     else:
-        # Free space
-        space_remaining = v
+        blocks.append([None, start_idx, size])
+    start_idx += size
 
+# Compact
+out_blocks = []
+for i, block in enumerate(blocks):
+    file_id, start_idx, size = block
+    if file_id is not None:
+        # Files always move over as-is
+        out_blocks.append(block)
+    else:
+        # Space, fill it
+        space_remaining = size
         while space_remaining > 0:
-            if tail_remaining == 0:
-                # No more file to move, get a new one
-                tail_cursor -= 2
-                tail_id -= 1
+            # Find a block to move
+            to_move = next(filter(lambda b: b[0] is not None, reversed(blocks)))
+            move_id, move_start, move_size = to_move
 
-                # Tail reached head
-                if tail_cursor <= i:
-                    break
-
-                tail_remaining = int(diskmap[tail_cursor])
-
-            if tail_remaining == 0:
+            # Only move backwards
+            if move_start <= start_idx:
                 break
 
-            # Moving file to head_out_idx
-            checksum += head_out_idx * tail_id
-            head_out_idx += 1
-            space_remaining -= 1
-            tail_remaining -= 1
+            if space_remaining >= move_size:
+                # Move whole block
+                out_blocks.append([move_id, start_idx, move_size])
+                space_remaining -= move_size
+                start_idx += move_size
 
+                # Now this block is a space
+                to_move[0] = None
+            else:
+                # Need to split block
+                out_blocks.append([move_id, start_idx, space_remaining])
+                to_move[2] -= space_remaining
+                space_remaining = 0
+
+checksum = 0
+for file_id, start_idx, size in out_blocks:
+    n = size * (start_idx + start_idx + size - 1) // 2
+    checksum += n * file_id
 print(checksum)
