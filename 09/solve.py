bf272febdfe40eb769b38f5d51ee36561b94b0df
Brian Starkey <stark3y@gmail.com>
2024-12-09 19:40:47 +0000

    09: part 1 - speed up
    Don't mutate stuff.

--- 09/solve.py
+++ 09/solve.py
@@ -11,17 +11,25 @@ start_idx = 0
 for i, v in enumerate(diskmap):
     size = int(v)
     if i % 2 == 0:
-        blocks.append([file_id, start_idx, size])
+        blocks.append((file_id, start_idx, size))
         file_id += 1
     else:
-        blocks.append([None, start_idx, size])
+        blocks.append((None, start_idx, size))
     start_idx += size
 
-# Compact
+# Compact (Part 1)
 out_blocks = []
+move_candidates = filter(lambda b: b[0] is not None, reversed(blocks))
+partial_block = None
+last_move_id = None
+
 for i, block in enumerate(blocks):
     file_id, start_idx, size = block
     if file_id is not None:
+        if file_id == last_move_id:
+            # Reached the last block
+            break
+
         # Files always move over as-is
         out_blocks.append(block)
     else:
@@ -29,26 +37,34 @@ for i, block in enumerate(blocks):
         space_remaining = size
         while space_remaining > 0:
             # Find a block to move
-            to_move = next(filter(lambda b: b[0] is not None, reversed(blocks)))
+            if partial_block is not None:
+                to_move = partial_block
+                partial_block = None
+            else:
+                to_move = next(move_candidates)
+
             move_id, move_start, move_size = to_move
 
             # Only move backwards
             if move_start <= start_idx:
                 break
 
+            last_move_id = move_id
+
             if space_remaining >= move_size:
                 # Move whole block
-                out_blocks.append([move_id, start_idx, move_size])
+                out_blocks.append((move_id, start_idx, move_size))
                 space_remaining -= move_size
                 start_idx += move_size
-
-                # Now this block is a space
-                to_move[0] = None
             else:
                 # Need to split block
-                out_blocks.append([move_id, start_idx, space_remaining])
-                to_move[2] -= space_remaining
+                out_blocks.append((move_id, start_idx, space_remaining))
+                partial_block = (move_id, move_start, move_size - space_remaining)
                 space_remaining = 0
+
+if partial_block is not None and file_id == partial_block[0]:
+    # Last block, is partial
+    out_blocks.append(partial_block)
 
 checksum = 0
 for file_id, start_idx, size in out_blocks:
