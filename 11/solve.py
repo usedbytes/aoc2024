124e263cb5f220745778427cd3f70cd2cf02ad1e
Brian Starkey <stark3y@gmail.com>
2024-12-11 08:41:51 +0000

    11: part 2
    Not too bad - we need to go depth first and memoise

--- 11/solve.py
+++ 11/solve.py
@@ -1,25 +1,35 @@
+import functools
 import math
 import sys
 
 with open(sys.argv[1]) as f:
     line = f.read().strip()
     stones = [int(v) for v in line.split()]
-print(stones)
 
-for blink in range(25):
-    new_stones = []
-    for stone in stones:
-        if stone == 0:
-            new_stones.append(1)
+@functools.cache
+def evaluate_stone(stone, nblinks):
+    if nblinks == 0:
+        return 1
+
+    if stone == 0:
+        return evaluate_stone(1, nblinks - 1)
+    else:
+        ndigits = len(str(stone))
+        if ndigits % 2 == 0:
+            bottom = stone % 10**(ndigits // 2)
+            top = stone // 10**(ndigits // 2)
+
+            a = evaluate_stone(top, nblinks - 1)
+            b = evaluate_stone(bottom, nblinks - 1)
+            return a + b
         else:
-            ndigits = len(str(stone))
-            if ndigits % 2 == 0:
-                bottom = stone % 10**(ndigits // 2)
-                top = stone // 10**(ndigits // 2)
-                new_stones.append(top)
-                new_stones.append(bottom)
-            else:
-                new_stones.append(stone * 2024)
-    stones = new_stones
+            return evaluate_stone(stone * 2024, nblinks - 1)
 
-print(len(stones))
+p1 = 0
+p2 = 0
+for stone in stones:
+    p1 += evaluate_stone(stone, 25)
+    p2 += evaluate_stone(stone, 75)
+
+print(p1)
+print(p2)
