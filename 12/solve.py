fdf5f6e325c134b7e298994225a7c1ff38265d55
Brian Starkey <stark3y@gmail.com>
2024-12-12 21:21:38 +0000

    12: part 1
    I didn't do a flood fill because I assume it won't work for part 2.

---
+++ 12/solve.py
@@ -0,0 +1,97 @@
+import sys
+
+from collections import namedtuple
+from dataclasses import dataclass, field
+from typing import List, Optional
+
+@dataclass
+class Slice:
+    color: str
+    start: int
+    end: Optional[int] = 0
+
+@dataclass
+class Blob:
+    color: str
+    perimeter: int = 0
+    area: int = 0
+    current_slices: List[Slice] = field(default_factory=list)
+    next_slices: List[Slice] = field(default_factory=list)
+
+p1 = 0
+with open(sys.argv[1]) as f:
+    current_blobs = []
+    for line in map(str.strip, f):
+        slices = []
+        current = None
+
+        # Create slices
+        for i, v in enumerate(line):
+            if current is None or current.color != v:
+                if current is not None:
+                    current.end = i
+                    slices.append(current)
+                current = Slice(color=v, start=i)
+
+        # Finish the last slice
+        if current is not None:
+            current.end = len(line)
+            slices.append(current)
+
+        # Match slices to blobs
+        next_blobs = []
+        for s in slices:
+            matching_blobs = []
+            total_overlap = 0
+            for blob in current_blobs:
+                if blob.color != s.color:
+                    continue
+
+                for ps in blob.current_slices:
+                    overlap = max(0, min(s.end, ps.end) - max(s.start, ps.start))
+                    if overlap > 0:
+                        if blob not in matching_blobs:
+                            matching_blobs.append(blob)
+                    total_overlap += overlap
+
+            if len(matching_blobs) > 0:
+                matched = matching_blobs[0]
+
+                # merge matching_blobs
+                for blob in matching_blobs[1:]:
+                    matched.area += blob.area
+                    matched.perimeter += blob.perimeter
+                    matched.current_slices.extend(blob.current_slices)
+                    # We're working left-to-right in slices, so no need
+                    # to extend next_slices
+
+                    current_blobs.remove(blob)
+                matched.perimeter += 2 + 2 * (s.end - s.start) - 2 * total_overlap
+                matched.area += s.end - s.start
+                matched.next_slices.append(s)
+            else:
+                # New blob
+                matched = Blob(
+                    color=s.color,
+                    perimeter=2 + 2 * (s.end - s.start),
+                    area=s.end-s.start,
+                    next_slices=[s],
+                )
+
+            if matched not in next_blobs:
+                next_blobs.append(matched)
+
+        for blob in next_blobs:
+            blob.current_slices = blob.next_slices
+            blob.next_slices = []
+
+        for blob in current_blobs:
+            if blob not in next_blobs:
+                p1 += blob.area * blob.perimeter
+        current_blobs = next_blobs
+
+    # All current blobs are now done
+    for blob in current_blobs:
+        p1 += blob.area * blob.perimeter
+
+print(p1)
