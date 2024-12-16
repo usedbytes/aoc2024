c5567b3d874b90bd9cbce2dda1b6677dc1c2a4e3
Brian Starkey <stark3y@gmail.com>
2024-12-16 23:09:05 +0000

    16: part 2
    An easy one for once!

--- 16/solve.py
+++ 16/solve.py
@@ -23,19 +23,26 @@ directions = [
     (0, -1),
 ]
 
-# q stores (cost, counter, pos, direction_idx)
+# q stores (cost, counter, pos, direction_idx, path)
 # counter just ensures ordering, as mentioned in the docs
-heapq.heappush(q, (0, i, start, 0))
+heapq.heappush(q, (0, i, start, 0, [start]))
 
 # Stores minimum cost to reach (x, y, dir) - note that including the direction
 # is important!
 min_cost = {}
 
+# Store all points on paths with the minimum cost
+on_path = set()
+
+p1 = None
 while q:
-    cost, _, (x, y), cur_dir = heapq.heappop(q)
+    cost, _, (x, y), cur_dir, path = heapq.heappop(q)
     if (x, y) == end:
-        p1 = cost
-        break
+        if p1 == None or cost <= p1:
+            p1 = cost
+            on_path.update(path)
+        # No need to break - we're only considering cheapest paths, so
+        # the queue is only the "frontier", it won't grow out of hand.
 
     for didx in range(len(directions)):
         dx, dy = directions[didx]
@@ -47,11 +54,17 @@ while q:
         new_cost = cost + (nturns * 1000) + 1
 
         if (nx, ny, didx) in min_cost:
-            if min_cost[(nx, ny, didx)] <= new_cost:
+            if min_cost[(nx, ny, didx)] < new_cost:
                 continue
         min_cost[(nx, ny, didx)] = new_cost
 
+        new_path = path[:]
+        new_path.append((nx, ny))
+
         i += 1
-        heapq.heappush(q, (new_cost, i, (nx, ny), didx))
+        heapq.heappush(q, (new_cost, i, (nx, ny), didx, new_path))
 
 print(p1)
+
+p2 = len(on_path)
+print(p2)
