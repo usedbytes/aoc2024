12114ffaa17b9c63e4f5b3c1dd7b3c3d2e1ab0e2
Brian Starkey <stark3y@gmail.com>
2024-12-16 21:49:42 +0000

    15: part 2
    Add a "check_only" mode to shove, so we can check the whole chain of
    moves for success before actually executing it.
    
    This means we recurse twice, but the input is pretty small so it's no
    problem.

--- 15/solve.py
+++ 15/solve.py
@@ -1,28 +1,53 @@
 import sys
 
-def shove(grid, start, direction):
+def shove(grid, start, direction, check_only = False):
+    #print("shove", grid, start, direction, check_only)
     x, y = start
     dx, dy = direction
     nx, ny = x + dx, y + dy
-
     hit = grid[ny][nx]
-    if hit == '.':
+
+    ok = False
+    match hit:
+        case ".":
+            ok = True
+        case "#":
+            ok = False
+        case "[":
+            if dy == 0:
+                # Moving horizontally, simple case
+                ok = shove(grid, (nx, ny), direction, check_only)
+            else:
+                # Moving vertically, do both
+                ok = (
+                    shove(grid, (nx, ny), direction, check_only) and
+                    shove(grid, (nx + 1, ny), direction, check_only)
+                )
+        case "]":
+            if dy == 0:
+                # Moving horizontally, simple case
+                ok = shove(grid, (nx, ny), direction, check_only)
+            else:
+                # Moving vertically, do both
+                ok = (
+                    shove(grid, (nx - 1, ny), direction, check_only) and
+                    shove(grid, (nx, ny), direction, check_only)
+                )
+        case _:
+            ok = shove(grid, (nx, ny), direction, check_only)
+
+    if ok and not check_only:
         this = grid[y][x]
         grid[ny][nx] = this
         grid[y][x] = '.'
-        return direction
-    elif hit == '#':
-        return (0, 0)
-    else:
-        result = shove(grid, (nx, ny), direction)
-        if result != (0, 0):
-            this = grid[y][x]
-            grid[ny][nx] = this
-            grid[y][x] = '.'
-        return result
+
+    return ok
 
 grid = []
 robot = None
+
+grid2 = []
+robot2 = None
 
 move_dirs = {
     "<": (-1, 0),
@@ -39,7 +64,7 @@ def calc_gps(grid):
     result = 0
     for y, row in enumerate(grid):
         for x, char in enumerate(row):
-            if char == "O":
+            if char == "O" or char == "[":
                 result += 100 * y + x
     return result
 
@@ -52,22 +77,51 @@ with open(sys.argv[1]) as f:
             break
 
         row = []
+        row2 = []
         for char in line:
-            if char == "@":
-                robot = (len(grid), len(row))
+            match char:
+                case "@":
+                    robot = (len(row), len(grid))
+                    robot2 = (len(row2), len(grid2))
+                    row2.append("@")
+                    row2.append(".")
+                case "O":
+                    row2.append("[")
+                    row2.append("]")
+                case _:
+                    row2.append(char)
+                    row2.append(char)
             row.append(char)
         grid.append(row)
+        grid2.append(row2)
 
-    #print_grid(grid)
+    moves = []
     try:
         while True:
             line = next(lines)
             for move in line:
-                direction = move_dirs[move]
-                rx, ry = shove(grid, robot, direction)
-                robot = (robot[0] + rx, robot[1] + ry)
-                #print_grid(grid)
+                moves.append(move)
     except StopIteration:
         pass
 
-print(calc_gps(grid))
+for move in moves:
+    direction = move_dirs[move]
+    moved = shove(grid, robot, direction)
+    if moved:
+        robot = (robot[0] + direction[0], robot[1] + direction[1])
+p1 = calc_gps(grid)
+print(p1)
+
+for move in moves:
+    direction = move_dirs[move]
+
+    # First check if everything is OK to move
+    can_move = shove(grid2, robot2, direction, True)
+
+    # Then do it
+    if can_move:
+        moved = shove(grid2, robot2, direction, False)
+        assert moved
+        robot2 = (robot2[0] + direction[0], robot2[1] + direction[1])
+p2 = calc_gps(grid2)
+print(p2)
