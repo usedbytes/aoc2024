b0e01b126f0530f09ad8b4aa93b2e72be792a04d
Brian Starkey <stark3y@gmail.com>
2024-12-18 08:20:52 +0000

    18: part 2
    Just brute force, wasn't too hard

--- 18/solve.py
+++ 18/solve.py
@@ -9,56 +9,64 @@ else:
     limit = 12
 grid = [[0] * width for i in range(height)]
 
-with open(sys.argv[1]) as f:
-    for i, line in enumerate(map(str.strip, f)):
-        if i >= limit:
-            break
-
-        x, y = map(int, line.split(","))
-        grid[y][x] = 1
-
 start = (0, 0)
 end = (width - 1, height - 1)
 
-q = []
-directions = [
-    (1, 0),
-    (0, 1),
-    (-1, 0),
-    (0, -1),
-]
+def run_maze(grid):
+    q = []
+    directions = [
+        (1, 0),
+        (0, 1),
+        (-1, 0),
+        (0, -1),
+    ]
 
-# q stores (cost, counter, pos)
-# counter just ensures ordering, as mentioned in the docs
-heapq.heappush(q, (0, i, start))
+    i = 0
 
-# Stores minimum cost to reach (x, y)
-min_cost = {}
+    # q stores (cost, counter, pos)
+    # counter just ensures ordering, as mentioned in the docs
+    heapq.heappush(q, (0, i, start))
 
-p1 = None
-while q:
-    cost, _, (x, y) = heapq.heappop(q)
-    if (x, y) == end:
-        p1 = cost
-        break
+    # Stores minimum cost to reach (x, y)
+    min_cost = {}
 
-    for didx in range(len(directions)):
-        dx, dy = directions[didx]
-        nx, ny = x + dx, y + dy
-        if nx < 0 or nx >= width or ny < 0 or ny >= height:
-            continue
+    while q:
+        cost, _, (x, y) = heapq.heappop(q)
+        if (x, y) == end:
+            return cost
 
-        if grid[ny][nx] == 1:
-            # Can't move
-            continue
-
-        new_cost = cost + 1
-        if (nx, ny) in min_cost:
-            if min_cost[(nx, ny)] <= new_cost:
+        for didx in range(len(directions)):
+            dx, dy = directions[didx]
+            nx, ny = x + dx, y + dy
+            if nx < 0 or nx >= width or ny < 0 or ny >= height:
                 continue
-        min_cost[(nx, ny)] = new_cost
 
-        i += 1
-        heapq.heappush(q, (new_cost, i, (nx, ny)))
+            if grid[ny][nx] == 1:
+                # Can't move
+                continue
 
-print(p1)
+            new_cost = cost + 1
+            if (nx, ny) in min_cost:
+                if min_cost[(nx, ny)] <= new_cost:
+                    continue
+            min_cost[(nx, ny)] = new_cost
+
+            i += 1
+            heapq.heappush(q, (new_cost, i, (nx, ny)))
+    return None
+
+with open(sys.argv[1]) as f:
+    for i, line in enumerate(map(str.strip, f)):
+        x, y = map(int, line.split(","))
+
+        grid[y][x] = 1
+
+        if i >= limit - 1:
+            distance = run_maze(grid)
+            if distance is None:
+                # Part 2
+                print(f"{x},{y}")
+                break
+            if i == limit - 1:
+                # Part 1
+                print(distance)
