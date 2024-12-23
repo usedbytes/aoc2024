c58940bf4cdb69ba05e0ce5bd936faf95298a451
Brian Starkey <stark3y@gmail.com>
2024-12-23 09:38:30 +0000

    23: part 2
    This is horrendously inefficient and ugly, but I'm frantically trying to
    get us out of the door for Christmas, so it will do.

--- 23/solve.py
+++ 23/solve.py
@@ -11,6 +11,25 @@ with open(sys.argv[1]) as f:
         nmap[a].add(b)
         nmap[b].add(a)
 
+def find_clique(a, nmap, clique=None, seen=None):
+    this = set(nmap[a])
+    this.add(a)
+
+    if seen is None:
+        seen = set()
+    if clique is None:
+        clique = this
+    else:
+        clique.intersection_update(this)
+
+    seen.add(a)
+
+    for b in set(clique):
+        if b not in seen and b in clique:
+            find_clique(b, nmap, clique, seen)
+
+    return clique
+
 triples = set()
 for a, al in nmap.items():
     if a[0] != "t":
@@ -31,3 +50,13 @@ for a, al in nmap.items():
 
 p1 = len(triples)
 print(p1)
+
+cliques = []
+for a in nmap.keys():
+    clique = find_clique(a, nmap)
+    cliques.append(clique)
+
+cliques.sort(key=lambda v: len(v))
+
+p2 = sorted(cliques[-1])
+print(",".join(p2))
